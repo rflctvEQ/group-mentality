f0557c294bf0685362ad0ab05b37f84c3fc99ced
Ian Everitt <ian.andrew.everitt@gmail.com>
2021-05-06 23:01:20 -0500

    Update moderatorRoutes.js

--- controllers/api/moderatorRoutes.js
+++ controllers/api/moderatorRoutes.js
@@ -1,12 +1,14 @@
-//* done for now
+//* done for now  
+
 
 const router = require('express').Router();
 const { Moderator, ApprovedUserPost, UserPost } = require('../../models');
-const withAuth = require('../../utils/auth');
+const modAuth = require('../../utils/modAuth');
 const { route } = require('./userRoutes');
 
 // route for '/api/moderator/'
-// this isn't necessary for MVP
+//* this isn't necessary for MVP
+// creates new Moderator 
 // router.post('/', withAuth, async (req, res) => {
 //   try {
 //     const newModerator = await Moderator.create({
@@ -21,7 +23,8 @@ const { route } = require('./userRoutes');
 // });
 
 // route for '/api/moderator/:id'
-// not necessary for MVP
+//* not necessary for MVP
+// deletes Moderator by id
 // router.delete('/:id', withAuth, async (req, res) => {
 //   try {
 //     const moderatorData = await Moderator.destroy({
@@ -42,108 +45,64 @@ const { route } = require('./userRoutes');
 //   }
 // });
 
-//* this one works!
-// route for '/api/users/'
-// router.post('/', async (req, res) => {
-//   try {
-//     const moderatorData = await Moderator.create(req.body);
-
-//     req.session.save(() => {
-//       req.session.moderator_id = moderatorData.id;
-//       req.session.logged_in = true;
-
-//       res.status(200).json(moderatorData);
-//     });
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
-
-router.post('/login', async (req, res) => {
-  try {
-    // const userData = await User.findOne({ where: { email: req.body.email } });
-    const moderatorData = await Moderator.findOne({
-      where: { email: req.body.email },
-    });
-
-    // if (!userData) {
-    //   res
-    //     .status(400)
-    //     .json({ message: 'Incorrect email or password, please try again.' });
-    //   return;
-    // };
-
-    if (!moderatorData) {
-      res
-        .status(400)
-        .json({
-          message: 'Incorrect moderator email or password, please try again.',
-        });
-      return;
-    }
-
-    // const validPassword = await userData.checkPassword(req.body.password);
-    const validModeratorPassword = await moderatorData.checkPassword(
-      req.body.password
-    );
-
-    // if (!validPassword) {
-    //   res
-    //     .status(400)
-    //     .json({ message: 'Incorrect email or password, please try again' });
-    //   return;
-    // };
-
-    if (!validModeratorPassword) {
-      res
-        .status(400)
-        .json({
-          message: 'Incorrect moderator email or password, please try again',
-        });
-      return;
-    }
-
-    // // if (userData) {
-    //   req.session.save(() => {
-    //     req.session.user_id = userData.id;
-    //     req.session.logged_in = true;
-
-    //     res.json({ user: userData, message: 'You are now logged in!' });
-    //   });
-    // };
-
-    req.session.save(() => {
-      req.session.moderator_id = moderatorData.id;
-      req.session.logged_in_moderator = true;
-
-      res.json({
-        user: moderatorData,
-        message: 'Welcome, moderator. You are now logged in!',
-      });
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 //* this works!
 // routing for creating new ApprovedUserPost
-router.post('/', withAuth, async (req, res) => {
+router.post('/', modAuth, async (req, res) => {
+  console.log('=====================')
+  console.log(req.body);
   try {
     const newApprovedUserPost = await ApprovedUserPost.create({
-      ...req.body,
-      moderatorId: req.session.moderatorId,
+      postTitle: req.body.postTitle,
+      postContent: req.body.postContent,
+      responseContent: req.body.responseContent,
+      moderatorId: req.session.moderatorId
     });
 
     res.status(200).json(newApprovedUserPost);
   } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  };
+});
+
+//* this works (i think?)
+// route for '/api/users/login'
+router.post('/login', async (req, res) => {
+  try {
+    const moderatorData = await Moderator.findOne({ where: { email: req.body.email }});
+
+    if (!moderatorData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect moderator email or password, please try again.'})
+      return;
+    };
+
+    const validModeratorPassword = await moderatorData.checkPassword(req.body.password);
+
+    if (!validModeratorPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect moderator email or password, please try again' });
+        return;
+    }
+
+      req.session.save(() => {
+        req.session.moderatorId = moderatorData.id;
+        req.session.logged_in = true;
+        req.session.logged_in_moderator = true;
+        
+        res.json({ moderator: moderatorData, message: 'Welcome, moderator. You are now logged in!' });
+      });
+
+  } catch (err) {
     res.status(400).json(err);
   }
 });
 
 //* this works!
-// routing for deleting user posts
-router.delete('/:id', withAuth, async (req, res) => {
+// routing for deleting user posts 
+router.delete('/:id', modAuth, async (req, res) => {
   console.log('===========');
   console.log(req);
   try {
@@ -152,18 +111,18 @@ router.delete('/:id', withAuth, async (req, res) => {
         id: req.params.id,
         //* i'm not sure if this is correct
         // moderatorId: req.session.moderatorId
-      },
+      }
     });
 
     if (!userPostData) {
       res.status(404).json({ message: 'No user post found with this id!' });
       return;
-    }
+    };
 
     res.status(200).json(userPostData);
   } catch (err) {
     res.status(500).json(err);
-  }
+  };
 });
 
 // deleting user/response posts on homepage
