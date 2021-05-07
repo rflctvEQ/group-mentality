7d336ec1b7d89995dd98c5eb883bd3a431475291
jamtrip7 <73972652+jamtrip7@users.noreply.github.com>
2021-05-06 21:39:12 -0400

    Updated README

--- controllers/api/moderatorRoutes.js
+++ controllers/api/moderatorRoutes.js
@@ -1,5 +1,4 @@
-//* done for now  
-
+//* done for now
 
 const router = require('express').Router();
 const { Moderator, ApprovedUserPost, UserPost } = require('../../models');
@@ -43,23 +42,107 @@ const { route } = require('./userRoutes');
 //   }
 // });
 
+//* this one works!
+// route for '/api/users/'
+// router.post('/', async (req, res) => {
+//   try {
+//     const moderatorData = await Moderator.create(req.body);
+
+//     req.session.save(() => {
+//       req.session.moderator_id = moderatorData.id;
+//       req.session.logged_in = true;
+
+//       res.status(200).json(moderatorData);
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
+
+router.post('/login', async (req, res) => {
+  try {
+    // const userData = await User.findOne({ where: { email: req.body.email } });
+    const moderatorData = await Moderator.findOne({
+      where: { email: req.body.email },
+    });
+
+    // if (!userData) {
+    //   res
+    //     .status(400)
+    //     .json({ message: 'Incorrect email or password, please try again.' });
+    //   return;
+    // };
+
+    if (!moderatorData) {
+      res
+        .status(400)
+        .json({
+          message: 'Incorrect moderator email or password, please try again.',
+        });
+      return;
+    }
+
+    // const validPassword = await userData.checkPassword(req.body.password);
+    const validModeratorPassword = await moderatorData.checkPassword(
+      req.body.password
+    );
+
+    // if (!validPassword) {
+    //   res
+    //     .status(400)
+    //     .json({ message: 'Incorrect email or password, please try again' });
+    //   return;
+    // };
+
+    if (!validModeratorPassword) {
+      res
+        .status(400)
+        .json({
+          message: 'Incorrect moderator email or password, please try again',
+        });
+      return;
+    }
+
+    // // if (userData) {
+    //   req.session.save(() => {
+    //     req.session.user_id = userData.id;
+    //     req.session.logged_in = true;
+
+    //     res.json({ user: userData, message: 'You are now logged in!' });
+    //   });
+    // };
+
+    req.session.save(() => {
+      req.session.moderator_id = moderatorData.id;
+      req.session.logged_in_moderator = true;
+
+      res.json({
+        user: moderatorData,
+        message: 'Welcome, moderator. You are now logged in!',
+      });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 //* this works!
 // routing for creating new ApprovedUserPost
 router.post('/', withAuth, async (req, res) => {
   try {
     const newApprovedUserPost = await ApprovedUserPost.create({
       ...req.body,
-      moderatorId: req.session.moderatorId
+      moderatorId: req.session.moderatorId,
     });
 
     res.status(200).json(newApprovedUserPost);
   } catch (err) {
     res.status(400).json(err);
-  };
+  }
 });
 
 //* this works!
-// routing for deleting user posts 
+// routing for deleting user posts
 router.delete('/:id', withAuth, async (req, res) => {
   console.log('===========');
   console.log(req);
@@ -69,18 +152,18 @@ router.delete('/:id', withAuth, async (req, res) => {
         id: req.params.id,
         //* i'm not sure if this is correct
         // moderatorId: req.session.moderatorId
-      }
+      },
     });
 
     if (!userPostData) {
       res.status(404).json({ message: 'No user post found with this id!' });
       return;
-    };
+    }
 
     res.status(200).json(userPostData);
   } catch (err) {
     res.status(500).json(err);
-  };
+  }
 });
 
 // deleting user/response posts on homepage
